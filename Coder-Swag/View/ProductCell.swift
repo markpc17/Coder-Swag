786aa29a7a9ad6e1b3e51bbc16579be43c013729
Mark Carruthers <markpc@Mark-Carrutherss-MacBook-Pro.local>
2018-08-21 20:21:13 +0100

    Collection Views

---
+++ Coder-Swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  Coder-Swag
+//
+//  Created by Mark Carruthers on 21/08/2018.
+//  Copyright © 2018 Mark Carruthers. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
