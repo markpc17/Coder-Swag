054f86ce78d35a781a2f3e039ab1af9732e11ac8
Mark Carruthers <markpc@Mark-Carrutherss-MacBook-Pro.local>
2018-08-21 19:45:12 +0100

    Project Created and Table View done

---
+++ Coder-Swag/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  Coder-Swag
+//
+//  Created by Mark Carruthers on 21/08/2018.
+//  Copyright © 2018 Mark Carruthers. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
